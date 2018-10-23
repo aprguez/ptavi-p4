37bbd2192d8b7d0766c9a7aa5afaad41f7095b9c
aprguez <alejandro_p-rguez@hotmail.com>
2018-10-23 19:32:05 +0200

    Ej 4 sin revisar

--- client.py
+++ client.py
@@ -3,20 +3,38 @@
 """
 Programa cliente UDP que abre un socket a un servidor
 """
-#esqueleto
-import socket
 
-# Constantes. Dirección IP del servidor y contenido a enviar
+import socket
+import sys
+
+"""
+Constantes. Dirección IP del servidor y contenido a enviar
+"""
+
+#Direccion IP
 SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
+
+        
+
+#Contenido a enviar    
+LINE = sys.argv[3]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
-
-print("Socket terminado.")
+    
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((SERVER, PORT))
+    
+    
+print("Enviando: " + LINE)
+my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+data = my_socket.recv(1024)
+        
+print('Recibido -- ', data.decode('utf-8'))
+print("Terminando socket. . .")
+    
+#cerrando la conexion
+my_socket.close()
+print("Closed.")
