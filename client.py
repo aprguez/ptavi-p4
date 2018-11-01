0805b5bdb163964e00d4cbfed6f33c4715eb6930
aprguez <alejandro_p-rguez@hotmail.com>
2018-11-01 21:02:45 +0100

    final con pep8

--- client.py
+++ client.py
@@ -1,36 +1,34 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-Programa cliente UDP que abre un socket a un servidor
+Programa cliente que abre un socket a un servidor
 """
 
 import socket
 import sys
 
-"""
-Constantes. Dirección IP del servidor y contenido a enviar
-"""
+if len(sys.argv) != 6:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
-#Direccion IP
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-PETICIONES = sys.argv[3]
-DIRECCIONSIP = sys.argv[4]
-EXPIRES = sys.argv[5]
+PETICION = sys.argv[3].upper()
+DIRECCION = sys.argv[4]
+TIEMPO = sys.argv[5]
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-    
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
- 
-print("Enviando: " + PETICIONES + " sip: " + DIRECCIONSIP + '\r\n' + "Expires: " + EXPIRES)
-my_socket.send(PETICIONES + " sip: " + DIRECCIONSIP  + " SIP/1.0 " + '\r\n' + "Expires: " + EXPIRES + '\r\n\r\n')
-data = my_socket.recv(1024) #tamaño del buffer
-        
+
+METODO = "REGISTER"
+PETICION = METODO + " sip:" + DIRECCION + " " + "SIP/2.0" + "\r\n" + "Expires: " + str(TIEMPO) + "\r\n" + "\r\n"
+print("Enviando: " + PETICION)
+my_socket.send(bytes(PETICION, 'utf-8'))
+data = my_socket.recv(1024)
+""" Tamaño del Buffer 1024 """
+
 print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket. . .")
-    
-#cerrando la conexion
+print("Terminando socket...")
+
 my_socket.close()
-print("Closed.")
+print("Fin.")
